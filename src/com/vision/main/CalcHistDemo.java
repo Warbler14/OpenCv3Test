f04d2291a896e2a98021bbc0585006b5df8f506e
Warbler14 <firefly11a@naver.com>
2019-11-30 09:56:13 +0900

    calcHist

---
+++ src/com/vision/main/CalcHistDemo.java
@@ -0,0 +1,20 @@
+package com.vision.main;
+
+import org.opencv.core.Core;
+
+import com.vision.process.CalcHist;
+
+public class CalcHistDemo {
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		String filename = args.length > 0 ? args[0] : getResourceFilePath("lena.jpg");
+		
+		
+		new CalcHist().run(filename);
+	}
+	
+	public static String getResourceFilePath(final String fileName) {		
+		return System.getProperty("user.dir")  + "\\resources\\" + fileName;
+	}
+}
